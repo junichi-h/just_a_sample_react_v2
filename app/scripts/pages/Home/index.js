96c9959c8efedc768609e17c2646189e3f45ae82
Junichi Honda <junichi@imgsrc.co.jp>
2018-09-12 12:22:13 +0900

    react-helmet追加

--- app/scripts/pages/Home/index.js
+++ app/scripts/pages/Home/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import style from './style.sass';
 
 const Home = () => (
   <div className={style.wrapper}>
+    <Helmet>
+      <title>Home</title>
+    </Helmet>
     <h2 className={style.title}>Home</h2>
   </div>
 );
--- app/scripts/pages/Login/index.js
+++ app/scripts/pages/Login/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import style from './style.sass';
 
 // Login
 const Login = () => (
   <div className={style.wrapper}>
+    <Helmet>
+      <title>Login</title>
+    </Helmet>
     <h2 className={style.title}>Login</h2>
   </div>
 );
